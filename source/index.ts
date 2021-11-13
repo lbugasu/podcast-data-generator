d72afffa5a38b1246163c26dc8d384699c1a9802
lbugasu <lbugasu@gmail.com>
2021-11-12 23:45:49 -0600

    handle errors

--- source/index.ts
+++ source/index.ts
@@ -1,4 +1,4 @@
-import { prepare, getRssFeedsFromOPML, findNamedEntities, writeToFile, getDataFromRssFeed } from './lib/helpers'
+import { prepare, getRssFeedsFromOPML, findNamedEntities, writeToFile, getDataFromRssFeed, logError } from './lib/helpers'
 import path from 'path'
 import { Podcast } from './models'
 import striptags from 'striptags';
@@ -15,14 +15,21 @@ async function download() {
 async function ner(rssFeedData: Podcast[]) {
   return Promise.all(rssFeedData.map(async (data) => {
     const entities = await findNamedEntities(data.description)
-    const episodesWithEntities = await Promise.all(data.items.map( async (episode) => {
+    let episodesWithEntities = await Promise.all(data.items.map( async (episode) => {
       const description = striptags(episode?.content || '')
-      const itemEntities = await findNamedEntities(description)
+      const itemEntities = await findNamedEntities(description).catch((error: any) => {
+        console.log('something mysterious happened: ', error.message)
+        logError(episode, error)
+      })
       episode.entities = itemEntities
       return episode
-    }))
+    })).catch((error: any) => {
+      console.log('error processing podcast: ', error.message)
+      episodesWithEntities = data.items
+      logError(data, error)
+    })
     data.entities = entities
-    data.items = episodesWithEntities
+    data.items = episodesWithEntities ?? data.items
     return data
   }))
 }
