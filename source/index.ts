2fb301901779129f66966ccd4cdd46a2e4051089
lbugasu <lbugasu@gmail.com>
2021-11-12 23:29:23 -0600

    working release

--- source/index.ts
+++ source/index.ts
@@ -7,8 +7,7 @@ const opmlFilePath = path.resolve(process.cwd(), './data/podcasts_opml.xml')
 
 async function download() {
   const feeds = await getRssFeedsFromOPML(opmlFilePath)
-  const _feeds = feeds.slice(0, 2)
-  return Promise.all(_feeds.map((feed) => {
+  return Promise.all(feeds.map((feed) => {
     return getDataFromRssFeed(feed.xmlurl)
   }))
 }
@@ -16,7 +15,7 @@ async function download() {
 async function ner(rssFeedData: Podcast[]) {
   return Promise.all(rssFeedData.map(async (data) => {
     const entities = await findNamedEntities(data.description)
-    const episodesWithEntities = await Promise.all(data.items.slice(0, 2).map( async (episode) => {
+    const episodesWithEntities = await Promise.all(data.items.map( async (episode) => {
       const description = striptags(episode?.content || '')
       const itemEntities = await findNamedEntities(description)
       episode.entities = itemEntities
