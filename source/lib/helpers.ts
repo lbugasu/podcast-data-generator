19c724be2eaa3169609894d2e29cf3f018aabf87
Laurence Ininda <lbugasu@gmail.com>
2021-11-13 01:42:10 -0600

    Update helpers.ts

--- source/lib/helpers.ts
+++ source/lib/helpers.ts
@@ -58,7 +58,7 @@ function logError(item: any, error: any){
 }
 
 function writeToFile(podcast: any) {
-  console.log(podcast)
+  console.log('saving: ', podcast?.title)
   const folder = process.cwd() + '/tmp/dist/podcasts'
   try {
     fs.writeFileSync(`${folder}/${podcast.title}.json`, JSON.stringify(podcast, null, 4), 'utf8')
