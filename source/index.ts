12d5ccf51eab913b6c17f84470edbecfe6c85392
Laurence Ininda <lbugasu@gmail.com>
2021-11-13 15:11:33 -0600

    progress

--- source/index.ts
+++ source/index.ts
@@ -1,21 +1,26 @@
-import { prepare, getRssFeedsFromOPML, findNamedEntities, writeToFile, getDataFromRssFeed, logError } from './lib/helpers'
 import path from 'path'
-import { Podcast } from './models'
+import slug from 'slug'
 import striptags from 'striptags'
+
+import { findNamedEntities, getDataFromXMLString, getFile, getFilesInFolder, getRssFeedsFromOPML, parseRssXMLString, PodcastFeedData, prepare, writeToFile } from './lib/helpers'
+import { Podcast } from './models'
 
 const opmlFilePath = path.resolve(process.cwd(), './data/podcasts_opml.xml')
 
-async function download() {
+async function downloadFeeds(): Promise<(PodcastFeedData | void)[]> {
   const feeds = await getRssFeedsFromOPML(opmlFilePath)
-  return Promise.all(
+  const podcasts =  Promise.all(
     feeds.map(async (feed: any, i: number) => {
-      return getDataFromRssFeed(feed.xmlurl)
+      return getDataFromXMLString(feed.xmlurl)
         .catch((error: any) => {
           console.log('error on url: ', feed.xmlurl)
           console.log(`error parsing rss feed- ${i}: `, error.message)
         })
     })
   )
+  const _podcsts = (await podcasts).filter((podcast: any) => podcast)
+  console.log(_podcsts.length)
+  return _podcsts
 }
 
 async function ner(rssFeedData: any[]) {
@@ -46,14 +51,59 @@ async function ner(rssFeedData: any[]) {
 function write(podcasts: any[]) {
   podcasts.forEach((podcast, i) => {
     if(!podcast) return
-    writeToFile(podcast, (i/podcasts.length))
+    writeToFile(podcast, podcast.title, 'podcasts', (i/podcasts.length))
   })
+}
+
+function saveFeedsToFolder(podData: any[]) {
+  podData.filter((pod)=>!!pod).forEach((pod: PodcastFeedData, i) => {
+    writeToFile(pod.feed, slug(pod.rssUrl), 'feeds', (i/podData.length))
+  })
+  return Promise.resolve(true)
+}
+
+function parsePodcastData() {
+  const pdcstRssFeedPaths = getFilesInFolder('feeds')
+  const pdcsts = Promise.all(pdcstRssFeedPaths.map(async (xmlFeedPath, i) => {
+    console.log(`parsing XML Feeds: ${ ((i/pdcstRssFeedPaths.length)*100).toFixed(2)}%`)
+    const xmlFeed = getFile(`feeds\/${xmlFeedPath}`)
+    const parsedRssFeed = await parseRssXMLString(xmlFeed)
+      .catch((error: any) => {
+      console.log('Error: ', error.message)
+    })
+    if (parsedRssFeed) {
+      console.log(`parsed ${i} of ${pdcstRssFeedPaths.length}`)
+      writeToFile(parsedRssFeed, slug(parsedRssFeed.title || xmlFeedPath), 'podcasts', (i/pdcstRssFeedPaths.length))
+    }
+    
+    return parsedRssFeed
+  }))
+  pdcsts.then((pdcsts) => console.log('DONE Writing Podcasts JSON Files'))
 }
 
 // Prepare folders for dist
 prepare()
 
+
 // Action
-download()
-  .then((rssFeedData: any[]) => ner(rssFeedData))
-  .then(podcasts => write(podcasts))
+downloadFeeds()
+  // .then((feeds: any[]) => {
+  //   try {  return saveFeedsToFolder(feeds)}
+  //   catch (error: any) {
+  //     console.log('error saving feeds to folder', error.message)
+  //     return Promise.resolve(false)
+  //   }
+  // })
+  // .then((result: boolean) => {
+  //   if( result ) {
+  //     console.log('done')
+  //     parsePodcastData()
+  //   }
+  //   else {
+  //     console.log('error')
+  //   }
+  // })
+// parsePodcastData()
+    
+  // .then((rssFeedData: any[]) => ner(rssFeedData))
+  // .then(podcasts => write(podcasts))
--- source/lib/helpers.ts
+++ source/lib/helpers.ts
@@ -1,9 +1,14 @@
 /* Imports */
 import fs from 'fs'
-import { opmlToJSON } from 'opml-to-json'
 import NerPromise, { Entity } from 'ner-promise'
-
+import { opmlToJSON } from 'opml-to-json'
+// import Parser from 'rss-parser'
 import Parser from 'rss-parser'
+
+import { Podcast } from './../models/index';
+
+const rssUrlParser = require('rss-url-parser')
+const parser = new Parser()
 
 const nerParser = new NerPromise({
   install_path: 'tmp/stanford-ner-2020-11-17'
@@ -16,9 +21,11 @@ async function getRssFeedsFromOPML(filePath: string): Promise<any[]> {
 }
 
 async function parseRssFeed(rssUrl: string) {
-  const parser = new Parser()
-  const feed = await parser.parseURL(rssUrl)
-  return feed
+  return parser.parseURL(rssUrl)
+}
+
+async function parseRssXMLString(xmlString: string) {
+  return parser.parseString(xmlString)
 }
 
 async function findNamedEntities(text?: string): Promise<Entity[]> {
@@ -36,12 +43,15 @@ async function findNamedEntities(text?: string): Promise<Entity[]> {
   else return Promise.resolve([])
 }
 
-async function getDataFromRssFeed(rssUrl: string) {
-  return parseRssFeed(rssUrl)
+export type PodcastFeedData = {feed: Podcast, rssUrl: string}
+
+async function getDataFromXMLString(rssUrl: string): Promise<PodcastFeedData> {
+  const feed = await parseRssFeed(rssUrl)
+  return { feed: feed, rssUrl: rssUrl }
 }
 
 function prepare() {
-  const filePath = process.cwd() + '/tmp/dist/logs.md'
+  const filePath = process.cwd() + '\/tmp\/dist/logs.md'
   const initLog = `## Data Collection and Generation\n\n`
   fs.writeFile(filePath, initLog, (err) => {
     if (err) console.log('error in logs: ', err.message)
@@ -50,7 +60,7 @@ function prepare() {
 }
 
 function logError(item: any, error: any){
-  const filePath = process.cwd() + '/tmp/dist/logs.md'
+  const filePath = process.cwd() + '\/tmp\/dist/logs.md'
   const logMessage = `\n\n${item?.title} failed to write to file: ${error.message}\n\n`
   fs.appendFile(filePath, logMessage, (err) => {
     if (err) console.log('error in logs: ', err.message)
@@ -58,16 +68,24 @@ function logError(item: any, error: any){
   })
 }
 
-function writeToFile(podcast: any, total: number) {
+function writeToFile(podcast: any, fileName?: string, folderName?: string,  total?: number) {
   // console.log('saving: ', podcast?.title)
-  const folder = process.cwd() + '/tmp/dist/podcasts'
+  const folder = process.cwd() + `\/tmp\/dist/${folderName}`
   try {
-    fs.writeFileSync(`${folder}/${podcast.title}.json`, JSON.stringify(podcast, null, 4), 'utf8')
-    console.log(`done ${(total * 100).toFixed(2)}% - ${podcast.title}.json`)
-  } catch (error: any) {
+    fs.writeFileSync(`${folder}/${fileName}.json`, JSON.stringify(podcast, null, 4), 'utf8')
+    if(total) console.log(`done ${(total * 100).toFixed(2)}% - ${fileName}.json`)
+  } catch (error) {
     logError(podcast, error)
   }
 
 }
 
-export { getRssFeedsFromOPML, findNamedEntities, getDataFromRssFeed, writeToFile, prepare, logError }
+export function getFilesInFolder(folderName: string) {
+  return fs.readdirSync(process.cwd() + `\/tmp\/dist/${folderName}`);
+}
+
+export function getFile(filePath: string) {
+  return fs.readFileSync(process.cwd() + `\/tmp\/dist/${filePath}`, 'utf8')
+}
+
+export { getRssFeedsFromOPML, findNamedEntities, getDataFromXMLString, writeToFile, prepare, parseRssXMLString, logError }
